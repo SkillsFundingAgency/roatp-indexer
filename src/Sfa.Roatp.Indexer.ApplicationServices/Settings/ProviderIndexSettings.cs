0859a3efaff79fa38b52fe67521a23658b099ebd
Scott Cowan <info@scottcowan.org>
2017-03-21 11:48:44 +0000

    forcing alias to be lower case

--- src/Sfa.Roatp.Indexer.ApplicationServices/Settings/ProviderIndexSettings.cs
+++ src/Sfa.Roatp.Indexer.ApplicationServices/Settings/ProviderIndexSettings.cs
@@ -13,7 +13,7 @@ namespace Sfa.Roatp.Indexer.ApplicationServices.Settings
             _settings = settingsProvider;
         }
 
-        public string IndexesAlias => string.Format(_settings.GetSetting("ElasticSearch.IndexAliasFormat"), _settings.GetSetting("WorkerRole.EnvironmentName"));
+        public string IndexesAlias => string.Format(_settings.GetSetting("ElasticSearch.IndexAliasFormat"), _settings.GetSetting("WorkerRole.EnvironmentName")).ToLower();
 
         public string PauseTime => _settings.GetSetting("WorkerRole.RunFrequencySeconds");
     }
