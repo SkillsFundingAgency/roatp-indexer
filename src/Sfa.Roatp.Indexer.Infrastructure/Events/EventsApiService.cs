d7c12dad22acfb6c467a11d4cf4c36df16b067e5
Scott Cowan <info@scottcowan.org>
2017-03-22 11:17:28 +0000

    logging when there is a new provider detected

--- src/Sfa.Roatp.Indexer.Infrastructure/Events/EventsApiService.cs
+++ src/Sfa.Roatp.Indexer.Infrastructure/Events/EventsApiService.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Sfa.Roatp.Indexer.ApplicationServices.Events;
 using SFA.DAS.Events.Api.Client;
 using SFA.DAS.Events.Api.Types;
+using SFA.DAS.NLog.Logger;
 
 namespace Sfa.Roatp.Indexer.Infrastructure.Events
 {
@@ -12,15 +13,19 @@ namespace Sfa.Roatp.Indexer.Infrastructure.Events
 
         private readonly EventsApi _client;
         private readonly IEventsApiSettings _eventsApiClientConfiguration;
+        private readonly ILog _log;
 
-        public EventsApiService(IEventsApiSettings eventsApiClientConfiguration)
+        public EventsApiService(IEventsApiSettings eventsApiClientConfiguration, ILog log)
         {
             _client = new EventsApi(eventsApiClientConfiguration);
             _eventsApiClientConfiguration = eventsApiClientConfiguration;
+            _log = log;
         }
 
         public Task NewProvider(string ukprn)
         {
+            _log.Info($"New provider {ukprn}");
+
             if (_eventsApiClientConfiguration.Enabled)
             {
                 var agreementEvent = new AgreementEvent { ContractType = NewRoatpProviderContractType, Event = NewRoatpProviderEvent, ProviderId = ukprn };
