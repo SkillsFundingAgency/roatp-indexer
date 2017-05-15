c5bf68243d32b4e9f4757d93388fb3b6e7482588
Scott Cowan <info@scottcowan.org>
2017-05-15 16:09:51 +0100

    CI-286 splitting the roatp tests into two

--- src/Esfa.Roatp.Xslx.IntegrationTests/RoatpXslxTests.cs
+++ src/Esfa.Roatp.Xslx.IntegrationTests/RoatpXslxTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sfa.Roatp.Indexer.ApplicationServices;
 using Sfa.Roatp.Indexer.Core.Models;
@@ -9,21 +10,34 @@ namespace Esfa.Roatp.Xslx.IntegrationTests
     [TestClass]
     public class RoatpXslxTests
     {
-        [TestMethod]
-        public void ShouldLoadValidSpreadsheet()
+        List<RoatpProvider> results;
+
+        [TestInitialize]
+        public void Init()
         {
             // Arrange
             var container = IoC.Initialize();
             var sut = container.GetInstance<IGetRoatpProviders>();
 
             // Act
-            var results = sut.GetRoatpData().ToList();
+            results = sut.GetRoatpData().ToList();
+        }
 
+        [TestMethod]
+        public void RoatpShouldntHaveUnknownProviders()
+        {
             // Assert
             var invalidTypes = results.Where(x => x.ProviderType == ProviderType.Unknown).ToList();
             Assert.AreEqual(0, invalidTypes.Count, $"There are {invalidTypes.Count} unknown providers [{string.Join(", ", invalidTypes.Select(x => x.Ukprn))}]" );
+        }
+
+        [TestMethod]
+        public void RoatpShouldntHaveInvalidUkprns()
+        {
+            // Assert
             var invalidUkprns = results.Where(x => x.Ukprn.Length != 8).ToList();
             Assert.AreEqual(0, invalidUkprns.Count, $"There are {invalidUkprns.Count} invalid ukprns [{string.Join(", ", invalidUkprns.Select(x => x.Ukprn))}]");
         }
+
     }
 }
