4e65edd1e617057ae3b667760cdfc09c80005378
Scott Cowan <info@scottcowan.org>
2017-05-17 14:51:20 +0100

    adding a ROATP xlsx test for duplicate ukprns

--- src/Esfa.Roatp.Xslx.IntegrationTests/RoatpXslxTests.cs
+++ src/Esfa.Roatp.Xslx.IntegrationTests/RoatpXslxTests.cs
@@ -39,5 +39,11 @@ namespace Esfa.Roatp.Xslx.IntegrationTests
             Assert.AreEqual(0, invalidUkprns.Count, $"There are {invalidUkprns.Count} invalid ukprns [{string.Join(", ", invalidUkprns.Select(x => x.Ukprn))}]");
         }
 
+        [TestMethod]
+        public void RoatpShouldntHaveAnyDuplicateUkprns()
+        {
+            var duplicates = results.GroupBy(x => x.Ukprn).Where(g => g.Count() > 1).Select(x => x.Key).ToList();
+            Assert.AreEqual(0, duplicates.Count, $"There are {duplicates.Count} duplicate ukprns [{string.Join(", ", duplicates)}]");
+        }
     }
 }
