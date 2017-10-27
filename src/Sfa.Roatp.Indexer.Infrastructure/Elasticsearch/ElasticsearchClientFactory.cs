d84dadd8b7788e1be15b2cce0692f910547c3d88
Josevi Agullo <josevi.agullo@gmail.com>
2017-10-27 11:00:27 +0100

    Connecting to Elk5 if FeatureToggle is enabled

--- src/Sfa.Roatp.Indexer.Infrastructure/Elasticsearch/ElasticsearchClientFactory.cs
+++ src/Sfa.Roatp.Indexer.Infrastructure/Elasticsearch/ElasticsearchClientFactory.cs
@@ -1,5 +1,8 @@
 ﻿using Elasticsearch.Net;
+using FeatureToggle.Core.Fluent;
 using Nest;
+using Sfa.Roatp.Indexer.Infrastructure.Extensions;
+using Sfa.Roatp.Indexer.Infrastructure.FeatureToggles;
 using Sfa.Roatp.Indexer.Infrastructure.Settings;
 
 namespace Sfa.Roatp.Indexer.Infrastructure.Elasticsearch
@@ -15,9 +18,31 @@ namespace Sfa.Roatp.Indexer.Infrastructure.Elasticsearch
 
         public IElasticClient GetElasticClient()
         {
+            if (Is<IgnoreSslCertificateFeature>.Enabled)
+            {
+                using (var settings = new ConnectionSettings(
+                    new StaticConnectionPool(_infrastructureSettings.ElasticServerUrls),
+                    new MyCertificateIgnoringHttpConnection()))
+                {
+                    SetDefaultSettings(settings);
+
+                    return new ElasticClient(settings);
+                }
+            }
+
             using (var settings = new ConnectionSettings(new StaticConnectionPool(_infrastructureSettings.ElasticServerUrls)))
             {
+                SetDefaultSettings(settings);
+
                 return new ElasticClient(settings);
+            }
+        }
+
+        private void SetDefaultSettings(ConnectionSettings settings)
+        {
+            if (Is<Elk5Feature>.Enabled)
+            {
+                settings.BasicAuthentication(_infrastructureSettings.ElasticUsername, _infrastructureSettings.ElasticPassword);
             }
         }
     }
---
+++ src/Sfa.Roatp.Indexer.Infrastructure/FeatureToggles/IgnoreSslCertificateFeature.cs
@@ -0,0 +1,8 @@
+﻿using FeatureToggle.Toggles;
+
+namespace Sfa.Roatp.Indexer.Infrastructure.FeatureToggles
+{
+    public sealed class IgnoreSslCertificateFeature : SimpleFeatureToggle
+    {
+    }
+}
