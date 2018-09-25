1d3f026ae5eda4ced045fa4bb964a52905386a60
Matt Derry <matt@goodoldmatt.com>
2018-09-25 16:28:55 +0100

    Updates settings classes to include events/nservicebus config

--- src/Sfa.Roatp.Indexer.WorkerRole/Settings/IWorkerRoleSettings.cs
+++ src/Sfa.Roatp.Indexer.WorkerRole/Settings/IWorkerRoleSettings.cs
@@ -4,5 +4,7 @@
     {
         string WorkerRolePauseTime { get; }
         string EventsNServiceBusConnectionString { get; }
+        string EventsNServiceBusEndpointName { get; }
+        bool EventsNServiceBusDevelopmentMode { get; }
     }
 }
