43faaf43bf906edf49419bbf34d896bab7917a8c
josevi.agullo <josevi.agullo@gmail.com>
2018-12-18 10:04:11 +0000

    Updating integration tests

---
+++ src/Esfa.Roatp.Xslx.IntegrationTests/GetRoatpProvidersIntegrationService.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Net;
+using Sfa.Roatp.Indexer.ApplicationServices;
+using Sfa.Roatp.Indexer.ApplicationServices.Settings;
+using SFA.DAS.NLog.Logger;
+
+namespace Esfa.Roatp.Xslx.IntegrationTests
+{
+	internal class GetRoatpProvidersIntegrationService : RoatpProvidersXlsxService, IGetRoatpProviders
+	{
+		public GetRoatpProvidersIntegrationService(IAppServiceSettings appServiceSettings, ILog log) 
+			: base(appServiceSettings, log)
+		{
+		}
+
+		public override Stream GetFileStream()
+		{
+			return File.Open(_appServiceSettings.VstsRoatpUrl, FileMode.OpenOrCreate);
+		}
+	}
+}
