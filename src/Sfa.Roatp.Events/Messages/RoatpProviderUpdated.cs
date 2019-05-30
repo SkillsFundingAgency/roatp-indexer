f2288f676323a8c5bd6f3f415baff5950ef75a6c
MarkFCain <mark.cain@digital.education.gov.uk>
2019-05-30 10:55:04 +0100

    APR-488 adding RefreshDate to index from source xlsx

--- src/Sfa.Roatp.Events/Messages/RoatpProviderUpdated.cs
+++ src/Sfa.Roatp.Events/Messages/RoatpProviderUpdated.cs
@@ -24,6 +24,7 @@ namespace Sfa.Roatp.Events
         public DateTime? StartDate { get; set; }
 
         public DateTime? EndDate { get; set; }
+        public DateTime? RefreshDate { get; set; }
 
         public bool CurrentlyNotStartingNewApprentices { get; set; }
     }
--- src/Sfa.Roatp.Indexer.Infrastructure/Elasticsearch/ElasticsearchMapper.cs
+++ src/Sfa.Roatp.Indexer.Infrastructure/Elasticsearch/ElasticsearchMapper.cs
@@ -25,6 +25,7 @@ namespace Sfa.Roatp.Indexer.Infrastructure.Elasticsearch
                 ContractedForNonLeviedEmployers = roatpProvider.ContractedForNonLeviedEmployers,
                 StartDate = roatpProvider.StartDate,
                 EndDate = roatpProvider.EndDate,
+                RefreshDate = roatpProvider.RefreshDate,
                 NewOrganisationWithoutFinancialTrackRecord = roatpProvider.NewOrganisationWithoutFinancialTrackRecord,
                 ParentCompanyGuarantee = roatpProvider.ParentCompanyGuarantee,
                 ProviderType = roatpProvider.ProviderType,
